7331f66e9e4b2ac31a5d3cf8cf9b4bf11e26fa15
Jadon Wagstaff <jadonw@gmail.com>
2016-10-28 15:04:21 -0600

    Initial upload

---
+++ public/js/chart.js
@@ -0,0 +1,4 @@
+//testing script
+d3.select("#chart").append("text")
+	.text("This is a test.").attr("x", "100").attr("y", "100");
+console.log("test");
---
+++ public/js/map.js
@@ -0,0 +1,36 @@
+//map loaded here
+
+// initial drawing of map
+function drawMap(world) {
+
+    var svg = d3.select("#map");
+    var scale0 = (svg.attr("width") - 1) / 2 / Math.PI;
+
+
+
+    var projection = d3.geoNaturalEarth().scale(150).translate([svg.attr("width")/2, svg.attr("height")/2]);
+
+
+    var path = d3.geoPath()
+        .projection(projection);
+
+
+    d3.json("data/world.json", function(json) {
+
+        console.log(json);
+        svg.selectAll("path")
+            .data(topojson.feature(json, json.objects.countries).features)
+            .enter()
+            .append("path")
+            .classed("countries", true)
+            .attr("d", path);
+
+    });
+
+}
+
+// load map data and initialize drawing map
+d3.json("data/world.json", function (error, world) {
+    if (error) throw error;
+    drawMap(world);
+});
