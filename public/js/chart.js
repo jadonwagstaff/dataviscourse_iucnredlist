47a6966296a2c94a9c8d40d2641cfeb248cd585a
dartii <u1111495@utah.edu>
2016-11-02 19:48:06 -0600

    Update chart.js

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,4 +1,23 @@
-//testing script
-d3.select("#chart").append("text")
-	.text("This is a test.").attr("x", "100").attr("y", "100");
-console.log("test");
+
+function Chart(data){
+	self = this;
+	self.data = data;
+};
+
+
+Chart.prototype.update = function(){
+		
+	//testing script
+	d3.select("#chart").append("text")
+		.text("This is a test.").attr("x", "100").attr("y", "100");
+	console.log("test");
+	console.log(self.data);
+	
+	var xScale = d3.scaleLinear()
+		.domain([0, self.data.length])
+		.range([0, 550]); //hardcoded svg width
+	
+	
+	
+	
+};
