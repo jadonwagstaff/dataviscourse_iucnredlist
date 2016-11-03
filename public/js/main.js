eec5a812a28110f5e3a91ccb1e0eb7a9c7ff6da8
dartii <u1111495@utah.edu>
2016-11-02 19:47:11 -0600

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
 //loads and initializes data
+d3.csv("data/summary.csv", function (error, data) {
+	console.log(data);
+	
+	//create objects
+	var visChart = new Chart(organizeData(data));
+	visChart.update();
+	//var visMap = new Map();
+});
+
+function organizeData(data){ //will arrange data to fit with desired chart display
+  return data;
+};
