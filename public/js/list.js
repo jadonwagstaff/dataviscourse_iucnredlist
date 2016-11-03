1803ea5df0e282befcccddb3daedfe16765940e3
Jadon Wagstaff <jadonw@gmail.com>
2016-11-02 20:36:52 -0600

    Added list.js and connected to map and chart.

---
+++ public/js/list.js
@@ -0,0 +1,30 @@
+
+
+function List(map, chart, countryData) {
+    var self = this;
+
+    self.map = map;
+    self.chart = chart;
+    self.countryData = countryData;
+    self.init();
+}
+
+
+// initialises the countryList
+List.prototype.init = function(){
+    var self = this;
+
+    var list = d3.select("#countryList").selectAll("li");
+
+    list.data(self.countryData)
+        .enter()
+        .append("li")
+        .text(function (d){
+            return d.Country
+        })
+        .on("click", function(d, i){
+            self.chart.update(i);
+            self.map.update(i);
+        })
+}
+
