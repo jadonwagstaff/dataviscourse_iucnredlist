bda75944e9ec17b7777d637f9b0fd03cd3026fbe
dartii <u1111495@utah.edu>
2016-11-19 14:22:16 -0700

    Update main.js
    added list filter capability

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,6 @@ function changeData(organize) {
 
     d3.csv("data/mammals.csv", function (error, data) {
 
-
         //create objects
         var map = new Map(data);
         var chart = new Chart(data, percentage);
@@ -27,3 +26,31 @@ function organizeData(data){ //will arrange data to fit with desired chart displ
 };
 
 changeData();
+
+
+//loads search function
+//using modified code from http://www.w3schools.com/howto/howto_js_filter_lists.asp
+function searchFunction() {
+    // Declare variables
+    var input, filter, ul, li, i;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("countryList");
+	li = ul.getElementsByTagName('li');
+
+	//console.log(li[0].className);
+	
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+		if(li[i].className == "list"){
+			if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+				li[i].style.display = "";
+			} else {
+				li[i].style.display = "none";
+			}
+		}else {
+			li[i].style.display = "none";
+		}
+        
+    }
+}
