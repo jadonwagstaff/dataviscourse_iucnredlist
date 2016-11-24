7c48a397a98e85ca6e60fcbe50eefbba0d06fa71
dartii <u1111495@utah.edu>
2016-11-23 17:54:31 -0700

    Update main.js
    fixed filter

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,30 @@ function loadData() {
 
 }
 
+//loads search function
+//using modified code from http://www.w3schools.com/howto/howto_js_filter_lists.asp
+function searchFunction() {
+    // Declare variables
+    var input, filter, ul, li, i;
+    input = document.getElementById('search');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("countryList");
+	li = ul.getElementsByTagName('li');
+
+	//console.log(li[0].className);
+	
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+		if(li[i].className == "list"){
+			if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+				li[i].style.display = "";
+			} else {
+				li[i].style.display = "none";
+			}
+		}else {
+			li[i].style.display = "none";
+		}
+        
+    }
+}
+
