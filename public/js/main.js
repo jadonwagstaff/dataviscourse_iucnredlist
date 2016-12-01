60b4993acc6d1f6963d7d598c007e7b6ab7ef548
Jadon Wagstaff <jadonw@gmail.com>
2016-12-01 13:19:09 -0700

    fixed search bug

--- public/js/main.js
+++ public/js/main.js
@@ -36,15 +36,11 @@ function searchFunction() {
 	
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
-		if(li[i].className == "list"){
 			if (li[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
 				li[i].style.display = "";
 			} else {
 				li[i].style.display = "none";
 			}
-		}else {
-			li[i].style.display = "none";
-		}
         
     }
 }
